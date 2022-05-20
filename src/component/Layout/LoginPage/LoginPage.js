0962bdcf1d2764bc2e2c27b76bc5114640854d7c
emptywhite1 <emptywhite2@gmail.com>
2022-05-20 14:28:24 +0700

    change login page UI

--- src/component/Layout/LoginPage/LoginPage.js
+++ src/component/Layout/LoginPage/LoginPage.js
@@ -1,52 +1,64 @@
 import React from "react";
-import { Box, OutlinedInput, Button, Container, Typography, Grid } from "@material-ui/core"
+import { Box, OutlinedInput, Button, Container, Typography, Grid, makeStyles } from "@material-ui/core"
 import { Link } from 'react-router-dom';
 
+const useStyles = makeStyles((theme) => ({
+    container: {
+        backgroundColor: "#f2f2f2",
+        height: "100vh",
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+
+    }
+}))
 function LoginPage() {
+    const classes = useStyles()
     return (
-        <div>
-            <Box px = {5} py = {10} bgcolor = "silver" color = "black">
-                <Grid container justify="flex-end">
-                    <Grid item xs={7}>
-                    <Typography variant="h2">Job Finder</Typography>
+
+        <Container className={classes.container} maxWidth="xl">
+            <Box paddingTop={6} marginTop={3} >
+                <Grid container justify="center">
+                    <Grid item xl>
+                        <Link style={{ color: "black", textDecoration: "none" }} to="/">
+                            <Typography variant="h3">Job Finder</Typography>
+                        </Link>
+
                     </Grid>
                 </Grid>
-             
-                <Container maxWidth="sm" style = {{marginTop: "50px"}}>
-                    <Box px = {5} bgcolor="white" color="black">
-                        <Grid container justify="center" style = {{marginBottom: "50px"}}>  
-                            <Typography variant='h5' style = {{ fontWeight: "600" }}> Sign In </Typography> 
+
+                <Container maxWidth="xl" fullWidth style={{ marginTop: "25px" }}>
+                    <Box px={5} py={3} bgcolor="white" color="black" style={{ width: "450px", borderRadius: "10px" }}>
+                        <Grid container justify="center" style={{ marginBottom: "50px" }}>
+                            <Typography variant='h5' style={{ fontWeight: "600" }}> Sign In </Typography>
                         </Grid>
-                        <Typography variant='h6' style = {{marginTop: "5px"}}> Email </Typography>
-                        <OutlinedInput color = "black" fullWidth></OutlinedInput>
-                        <Typography variant='h6' style = {{marginTop: "10px"}}> Password </Typography>
-                        <OutlinedInput color = "black" fullWidth></OutlinedInput>
+                        <Typography variant='h6' style={{ marginTop: "5px" }}> Email </Typography>
+                        <OutlinedInput color="black" fullWidth></OutlinedInput>
+                        <Typography variant='h6' style={{ marginTop: "10px" }}> Password </Typography>
+                        <OutlinedInput color="black" fullWidth></OutlinedInput>
 
-                        <Link style = {{color: "black", textDecoration: "none"}} to = "/">
+                        <Link style={{ color: "black", textDecoration: "none" }} to="/">
                             <Grid container justify="center"  >
-                                <Button variant="contained" color="primary" style={{ marginTop: "10px" }} disableElevation>Login</Button>
+                                <Button variant="contained" color="primary" fullWidth style={{ marginTop: "20px",borderRadius: "20px" }} disableElevation>Login</Button>
                             </Grid>
                         </Link>
 
-                        <Link style = {{color: "black", textDecoration: "none"}} to = "/">
-                            <Grid container justify="center" style={{ marginTop: "10px" }} >
-                                <Button variant="contained" color="primary" disableElevation>Return to Home Page</Button>
-                            </Grid>
-                        </Link>
-                        
+
+
                         <Container maxWidth="sm">
                             <Box bgcolor="white" color="black" style={{ marginTop: "50px" }}>
-                                <Grid container justify="center" > 
-                                    <Typography style={{ marginTop: "10px" }}>Haven't got an account? <Link to = "/signup" style = {{color: "blue", textDecoration: "none"}}>
-                                        <Typography style={{ marginBottom: "10px", fontWeight: "600" }}>Create a new one now!</Typography> </Link> 
+                                <Grid container justify="center" >
+                                    <Typography style={{ marginTop: "10px" }}>Haven't got an account? <Link to="/signup" style={{ color: "blue", textDecoration: "none" }}>
+                                        <Typography style={{ marginBottom: "10px", fontWeight: "600" }}>Create a new one now!</Typography> </Link>
                                     </Typography>
                                 </Grid>
                             </Box>
                         </Container>
                     </Box>
                 </Container>
-            </Box> 
-        </div>
+            </Box>
+        </Container>
+
     );
-  };
-  export default LoginPage;
+};
+export default LoginPage;
