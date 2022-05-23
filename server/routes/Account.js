2577b20d322d6f0816d4c812ac5b15e8edb9249e
Nguyễn Thế Kiên <henekobbs123@gmail.com>
2022-05-23 11:04:51 +0700

    hash password, fixed [ERR_HTTP_HEADERS_SENT] run npm install bcrypt if code not working

--- server/routes/Account.js
+++ server/routes/Account.js
@@ -1,35 +1,43 @@
 const express = require("express");
 const router = express.Router();
 const { Account } = require("../models");
-
+const bcrypt = require("bcrypt")
 // router.get("/", async (req, res) => {
 //     const listOfAccount = await Account.findAll();
 //     res.json(listOfAccount);
 //     ;
 // });
 
-router.post("/", async (req, res) => {
+router.post("/", async(req, res) => {
     const {email, password} = req.body;
-    await Account.create({
-        email : email,
-        password: password,
-    });
-    res.json("success");
-});
+    bcrypt.hash(password, 10).then((hash) => {
+        Account.create({
+            email : email,
+            password: hash,
+        });
+    })
+})
 
 router.post("/login", async (req, res) => {
     const {email, password} = req.body;
     const account = await Account.findOne({where: {email : email}});
     if(!account) {
-        res.json({error:"account doesn't exist"});
+        return res.status(400).json({
+            status_code: 0,
+            error_msg: "Account Does Not Exist!",
+          });
     }
 
-    const passwordCheck = await Account.findOne({where : {password : password}})
-    if(!passwordCheck) {
-        res.json({error: "Wrong Pass Word!"})
-        
-    }
-    res.json("Login Success!");
+    bcrypt.compare(password, account.password).then((match) => {
+        if(!match) return res.status(400).json({
+            status_code: 0,
+            error_msg: "Wrong Password!",
+          });
+
+        res.json("Login Success!");
+    })
+    
+    
 });
 
 module.exports = router;
